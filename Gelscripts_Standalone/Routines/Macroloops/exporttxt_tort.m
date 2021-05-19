b75e2bfbb01b07f99b306a19b5b2256df8214da4
jimmink <80682535+jimmink@users.noreply.github.com>
2021-05-19 15:53:36 +0200

    Update exporttxt_tort.m

--- Gelscripts_Standalone/Routines/Macroloops/exporttxt_tort.m
+++ Gelscripts_Standalone/Routines/Macroloops/exporttxt_tort.m
@@ -1,27 +1,25 @@
-function exporttxt_tort(tort_data,dtortuosity,dxpath,dypath,dzpath,dxeucl,dyeucl,dzeucl,dxtortuosity,dytortuosity,dztortuosity,switch_tortinfo,pathstr,fname)
+function exporttxt_tort(tort_data,dtortuosity,dxpath,dypath,dzpath,dxeucl,dyeucl,dzeucl,dxtortuosity,dytortuosity,dztortuosity,pathstr,fname)
 
-if strcmp(switch_tortinfo,'yes')
-    fileidx = fopen([pathstr,'/Output/',fname,'/tortuosity_xpaths.txt'],'w');
-    fprintf(fileidx, 'Euclidean distance\tGel path distance\tTortuosity\n');
-    for i = 1:size(dxtortuosity,1)
-        fprintf(fileidx,[num2str(dxeucl(i)),'\t',num2str(dxpath(i)),'\t',num2str(dxtortuosity(i)),'\n']);
-    end
-    fclose(fileidx);
-
-    fileidy = fopen([pathstr,'/Output/',fname,'/tortuosity_ypaths.txt'],'w');
-    fprintf(fileidy, 'Euclidean distance\tGel path distance\tTortuosity\n');
-    for i = 1:size(dytortuosity,1)
-        fprintf(fileidy,[num2str(dyeucl(i)),'\t',num2str(dypath(i)),'\t',num2str(dytortuosity(i)),'\n']);
-    end
-    fclose(fileidy);
-
-    fileidz = fopen([pathstr,'/Output/',fname,'/tortuosity_zpaths.txt'],'w');
-    fprintf(fileidz, 'Euclidean distance\tGel path distance\tTortuosity\n');
-    for i = 1:size(dztortuosity,1)
-        fprintf(fileidz,[num2str(dzeucl(i)),'\t',num2str(dzpath(i)),'\t',num2str(dztortuosity(i)),'\n']);
-    end
-    fclose(fileidz);
+fileidx = fopen([pathstr,'/Output/',fname,'/tortuosity_xpaths.txt'],'w');
+fprintf(fileidx, 'Euclidean distance\tGel path distance\tTortuosity\n');
+for i = 1:size(dxtortuosity,1)
+    fprintf(fileidx,[num2str(dxeucl(i)),'\t',num2str(dxpath(i)),'\t',num2str(dxtortuosity(i)),'\n']);
 end
+fclose(fileidx);
+
+fileidy = fopen([pathstr,'/Output/',fname,'/tortuosity_ypaths.txt'],'w');
+fprintf(fileidy, 'Euclidean distance\tGel path distance\tTortuosity\n');
+for i = 1:size(dytortuosity,1)
+    fprintf(fileidy,[num2str(dyeucl(i)),'\t',num2str(dypath(i)),'\t',num2str(dytortuosity(i)),'\n']);
+end
+fclose(fileidy);
+
+fileidz = fopen([pathstr,'/Output/',fname,'/tortuosity_zpaths.txt'],'w');
+fprintf(fileidz, 'Euclidean distance\tGel path distance\tTortuosity\n');
+for i = 1:size(dztortuosity,1)
+    fprintf(fileidz,[num2str(dzeucl(i)),'\t',num2str(dzpath(i)),'\t',num2str(dztortuosity(i)),'\n']);
+end
+fclose(fileidz);
 
 fileidsum = fopen([pathstr,'/Output/',fname,'/tortuosity_summarized.txt'],'w');
 fprintf(fileidsum, 'Average tortuosity\tStandard Deviation\n');
