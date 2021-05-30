57f0ebc5d0a394b1f5339875d1f9ddfd9a5ae30f
jimmink <80682535+jimmink@users.noreply.github.com>
2021-05-30 21:50:34 +0200

    Delete Gelscripts_GUI directory

--- Gelscripts_GUI/Routines/Visualization/visualize_opt_prep.m
+++
@@ -1,6 +0,0 @@
-function visualize_opt_prep(Iraw, Ibin, Ifl, pathstr, fname)
-    
-    vis_bin_raw(Iraw,Ibin,[pathstr,'/Output/',fname,'/Rawimage_binarized.tif'])
-    vis_bin_bin(Ibin,Ifl,[pathstr,'/Output/',fname,'/Binarized_cleaning.tif'])
-    
-end
