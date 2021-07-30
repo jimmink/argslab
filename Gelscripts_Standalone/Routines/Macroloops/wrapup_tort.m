a1b23b79f82a4b11be46f2cfcfc6c2f699b2bb1b
jimmink <80682535+jimmink@users.noreply.github.com>
2021-07-30 16:09:15 +0200

    Delete Gelscripts_Standalone directory

--- Gelscripts_Standalone/Routines/Macroloops/wrapup_tort.m
+++
@@ -1,6 +0,0 @@
-function wrapup_tort(tort_summary,pathstr)
-
-fileid = fopen([pathstr,'/Output/Tortuosity_combined.txt'],'w');
-fprintf(fileid, 'Tortuosity\tStandard Deviation\n');
-fprintf(fileid, [num2str(tort_summary(1,1)),'\t',num2str(tort_summary(1,2)),'\n']);
-fclose(fileid);
--- Gelscripts_Standalone/Routines/Visualization/writetiff_bin.m
+++
@@ -1,6 +0,0 @@
-function writetiff_bin(img,filename)
-
-imwrite(img(:,:,1), filename)
-for i = 2:size(img,3)
-    imwrite(img(:,:,i), filename, 'writemode', 'append');
-end
